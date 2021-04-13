add5984cf388fc42ac162d852b666df7c1cf0c58
vitorcool@gmail.com <vitorcool@gmail.com>
2021-04-14 00:00:38 +0100

    avatar...hope last one

--- lib/hooks/useUserAvatar.ts
+++ lib/hooks/useUserAvatar.ts
@@ -6,17 +6,20 @@ export const useUserAvatar = (name = 'userAvatar') => {
   const { userAvatar, setUserAvatar } = useUI()
 
   useEffect(() => {
-    if (!userAvatar && localStorage.getItem(name)) {
+    /* if (!userAvatar && localStorage.getItem(name)) {
       // Get bg from localStorage and push it to the context.
       setUserAvatar(localStorage.getItem(name))
     }
     if (!localStorage.getItem(name)) {
       // bg not set locally, generating one, setting localStorage and context to persist.
       const bg = getRandomPairOfColors()
-      const value = '/user.svg'//linear-gradient(140deg, ${bg[0]}, ${bg[1]} 100%)`
+      const value = `linear-gradient(140deg, ${bg[0]}, ${bg[1]} 100%)`
       localStorage.setItem(name, value)
       setUserAvatar(value)
-    }
+    } */
+
+    // default avatar as backgroundimage
+    !userAvatar && setUserAvatar('/user.svg')
   }, [userAvatar])
 
   return {
